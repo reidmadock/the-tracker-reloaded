a3eda94381c66483fd0a5d8c4afd3e7d49f39905
Reid M <reidmadock@gmail.com>
2023-04-19 17:59:29 -0500

    Early server.js file

--- server.js
+++ server.js
@@ -1,2 +1,67 @@
 const inquirer = require('inquirer');
-const db = require('mysql2');
+const mysql = require('mysql2');
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'hr_db'
+    },
+    console.log(`Connected to the db`)
+);
+
+const options = [
+    'Exit application',
+    'View all departments',
+    'View all roles',
+    'View all employees',
+    'Add a department',
+    'Add a role',
+    'Add an employee',
+    'Update an employee\'s role'
+];
+
+function init() {
+    inquirer
+        .prompt([{
+            type: 'list',
+            name: 'actions',
+            loop: 'false',
+            message: 'What would you like to do?',
+            choices: [
+                options[1],
+                options[2],
+                options[3],
+                options[4],
+                options[5],
+                options[6],
+                options[7],
+                options[0],
+            ],
+            validate(val) {
+                if(val === options[0]) {
+                    console.info('exiting application...')
+                }
+            }
+        },
+    ])
+    .then((answer) => {
+        console.info(answer);
+    });
+    console.info('Am I fucking stuck here?');
+}
+
+async function testQuery() {
+    const result = await db.query('SELECT * FROM employee', (err, results) => {
+        if (err) {
+            console.info(err);
+        } else {
+            console.info(results);
+            return results;
+        }
+    });
+    return result;
+}
+
+init();
